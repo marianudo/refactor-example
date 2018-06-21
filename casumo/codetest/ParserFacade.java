ed618952244b03086f73efd0085d86f5e9f46a64
Mariano Navas <contact@marianonavas.me>
2018-06-21 20:08:45 +0200

    Expose public contract in the interface
    Declare the public methods of our ParserFacade in it's interface, so
    they can be accessed through the interface reference returned by the
    getInstance() factory method.

--- casumo/codetest/ParserFacade.java
+++ casumo/codetest/ParserFacade.java
@@ -1,9 +1,16 @@
 package casumo.codetest;
 
 import java.io.File;
+import java.io.IOException;
 
 public interface ParserFacade {
     default ParserFacade getInstance(File file) {
         return new ParserFacadeImpl(file);
     }
+
+    String getContent() throws IOException;
+
+    String getContentWithoutUnicode() throws IOException;
+
+    void saveContent(String content) throws IOException;
 }
